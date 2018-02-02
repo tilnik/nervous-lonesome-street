72f9477e855c1a7703f69946a6d4f9f65187054c
Gregor Slokan <gregor.slokan@medius.si>
2018-02-02 22:44:10 +0100

    logging

--- src/main/java/si/tilnik/nls/rest/RestAPI.java
+++ src/main/java/si/tilnik/nls/rest/RestAPI.java
@@ -1,23 +1,16 @@
 package si.tilnik.nls.rest;
 
-import java.sql.Connection;
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.naming.InitialContext;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
 
 import si.tilnik.nls.beans.NlsIssueTypeManager;
 import si.tilnik.nls.entity.NlsIssueType;
@@ -29,8 +22,7 @@ import si.tilnik.nls.entity.NlsIssueType;
 @Stateless
 public class RestAPI
 {
-//    @Inject
-//    private EntityManager em;
+    private Logger log = Logger.getLogger(RestAPI.class);
     @EJB
     private NlsIssueTypeManager nlsIssueTypeManager;
 
@@ -48,46 +40,47 @@ public class RestAPI
     @Path("/status")
     public String status()
     {
+        log.debug("Invoked: get status ... It works");
         return "It works!";
     }
 
-//    @GET
-//    @Produces("text/plain")
-//    public String root()
-//    {
-//        InitialContext context = null;
-//        Connection connection = null;
-//        String username = null;
-//        try
-//        {
-//            context = new InitialContext();
-//            DataSource ds = (DataSource) context.lookup("java:jboss/datasources/NlsDS");
-//            connection = ds.getConnection();
-//            username = connection.getMetaData().getUserName();
-//        }
-//        catch (RuntimeException e)
-//        {
-//            throw e;
-//        }
-//        catch (Exception e)
-//        {
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//        finally
-//        {
-//            if (connection != null)
-//            {
-//                try
-//                {
-//                    connection.close();
-//                }
-//                catch (Exception e)
-//                {
-//                    throw new RuntimeException(e.getMessage(), e);
-//                }
-//            }
-//        }
-//        return "Howdy " + username + " at " + new Date() + ".  ";
-//    }
+    //    @GET
+    //    @Produces("text/plain")
+    //    public String root()
+    //    {
+    //        InitialContext context = null;
+    //        Connection connection = null;
+    //        String username = null;
+    //        try
+    //        {
+    //            context = new InitialContext();
+    //            DataSource ds = (DataSource) context.lookup("java:jboss/datasources/NlsDS");
+    //            connection = ds.getConnection();
+    //            username = connection.getMetaData().getUserName();
+    //        }
+    //        catch (RuntimeException e)
+    //        {
+    //            throw e;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            throw new RuntimeException(e.getMessage(), e);
+    //        }
+    //        finally
+    //        {
+    //            if (connection != null)
+    //            {
+    //                try
+    //                {
+    //                    connection.close();
+    //                }
+    //                catch (Exception e)
+    //                {
+    //                    throw new RuntimeException(e.getMessage(), e);
+    //                }
+    //            }
+    //        }
+    //        return "Howdy " + username + " at " + new Date() + ".  ";
+    //    }
 }
 
